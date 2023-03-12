1cbead6bfbd77f3fb044e00641aa4b11b72afcf0
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-03-13 00:14:47 +0300

    task update

--- models/review.py
+++ models/review.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Module for Review class."""
+""" Review module """
+
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class representing a Review."""
+    """ Review class """
+
     place_id = ""
     user_id = ""
     text = ""
+
+    def __init__(self, *prmArgs, **prmKwArgs):
+        """
+            Constructor
+        """
+        super().__init__(*prmArgs, **prmKwArgs)
